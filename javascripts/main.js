97d7825169ca11d427a783c63613c6e2a6400379
Isabel Ramos <iramos1226@gmail.com>
2017-05-09 19:49:33 -0500

    added nav and body controllers

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
-console.log("yoo.");
+var app = angular.module("TodoApp", []);
+
+app.controller("NavCtrl", ($scope) => {
+	$scope.cat = "Meow";
+	$scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Items"}];
+
+});
+
+app.controller("ItemCtrl", ($scope) => {
+	$scope.dog = "Woof!";
+});
