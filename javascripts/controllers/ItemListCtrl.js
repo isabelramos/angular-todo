f1e0f0163417485d39798c0a6ad536f595b1d18a
Isabel Ramos <iramos1226@gmail.com>
2017-05-15 21:35:04 -0500

    added factories

--- javascripts/controllers/ItemListCtrl.js
+++ javascripts/controllers/ItemListCtrl.js
@@ -1,31 +1,10 @@
-app.controller("ItemListCtrl", function($http, $q, $scope, FIREBASE_CONFIG) {
-  $scope.items = [];
+app.controller("ItemListCtrl", function($scope, ItemFactory) {
+	$scope.items = [];
 
-  let getItemList = () => {
-    let itemz = [];
-    return $q ((resolve, reject) => {
-      $http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
-      .then((firebaseItems) => {
-          let itemCollection = firebaseItems.data;
-          Object.keys(itemCollection).forEach((key) => {
-            itemCollection[key].id = key;
-            itemz.push(itemCollection[key]);
-          });
-          resolve(itemz);
-      }).catch((error) => {
-        reject(error);
-      });
-    }); 
-  };
-
-  let getItems = () => {
-    getItemList().then((itemz) => {
+ 	ItemFactory.getItemList().then((itemz) => {
       $scope.items = itemz;
     }).catch((error) => {
       console.log("error", error);
     });
-  };
-
-  getItems();
 
 });
